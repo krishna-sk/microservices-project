2c735148eb49d6027c49ace3065a48ae35617baf
krishna <krishna.sk7991@gmail.com>
2021-12-18 08:57:20 +0530

    Config Server

--- product-service/src/main/java/com/krishna/ProductServiceApplication.java
+++ product-service/src/main/java/com/krishna/ProductServiceApplication.java
@@ -3,11 +3,13 @@ package com.krishna;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
